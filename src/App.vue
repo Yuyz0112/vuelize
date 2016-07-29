4b22b420cb95d124a4150721ea166f47fb65f1a2
YUyz <569812520@qq.com>
2016-07-29 14:42:40 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <img class="logo" src="./assets/vuelize.png">
+    <h1 class="title text-center">Vuelize.js</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Hello World!'
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+  @import "./assets/sass/milligram"
+  .logo
+    display: block
+    margin: 0 auto
+</style>
